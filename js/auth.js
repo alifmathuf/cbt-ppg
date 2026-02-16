24be190963f01cd2f8f55a4b9c5824d7d76947ae
alifmbs <146939780+alifmathuf@users.noreply.github.com>
2026-02-16 23:31:15 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,35 +1,18 @@
-/* ========= AUTH SYSTEM ========= */
-
-// generate ID sederhana
-function generateUserId() {
-  return "U" + Math.random().toString(36).substring(2, 9);
-}
-
-// login dari form
-function login() {
+function login(){
   const nama = document.getElementById("nama").value.trim();
   const kelas = document.getElementById("kelas").value.trim();
 
-  if (!nama || !kelas) {
-    alert("Isi nama dan mapel");
+  if(!nama || !kelas){
+    alert("Nama dan mapel wajib diisi");
     return;
   }
 
   const user = {
-    id: generateUserId(),
-    name: nama,
-    kelas: kelas
+    nama,
+    kelas
   };
 
-  localStorage.setItem("user", JSON.stringify(user));
+  localStorage.setItem("cbtUser", JSON.stringify(user));
 
   window.location.href = "dashboard.html";
-}
-
-// auto redirect jika sudah login
-function autoRedirect() {
-  const user = localStorage.getItem("user");
-  if (user) {
-    window.location.href = "dashboard.html";
-  }
 }
